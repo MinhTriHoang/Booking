e723844615a8debf95f77a8a01111bf081174b8d
MinhTriHoang <hoang45@purdue.edu>
2023-08-04 00:43:57 +0700

    Preparing for deployment to Heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 const pathToBuild = path.join(__dirname, 'my-app/src');
